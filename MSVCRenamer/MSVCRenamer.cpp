6a5dbc9def3885c8ebde0d571a5a3f64906da1f3
AnodicRave <maliceva786@gmail.com>
2026-04-23 01:29:32 +0300

    Добавьте файлы проекта.

---
+++ MSVCRenamer/MSVCRenamer.cpp
@@ -0,0 +1,12 @@
+﻿// MSVCRenamer.cpp: определяет точку входа для приложения.
+//
+
+#include "MSVCRenamer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
